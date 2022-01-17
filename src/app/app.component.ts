ce79d70790c8dfbc44c25808f673bcd97cfb861a
George Prince <prince.george35@gmail.com>
2022-01-17 06:20:35 -0600

    Github portfolio bare clean; Liners in Film rough prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
   public appPages = [
+    {
+      title: 'Home',
+      url: '',
+      icon: 'boat'
+    },
     {
       title: 'Login',
       url: '/login',
       icon: 'log-in'
-    },
-    {
-      title: 'Logout',
-      url: '/logout',
-      icon: 'log-out'
     },
     {
       title: 'Register',
@@ -26,10 +28,15 @@ export class AppComponent {
       url: '/users',
       icon: 'people'
     },
-    { title: 'Articles',
+    { title: 'Liners in Film Database',
       url: '/articles', 
       icon: 'newspaper'
     },
+    {
+      title: 'Logout',
+      url: '/logout',
+      icon: 'log-out'
+    }
   ]; 
   //  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
